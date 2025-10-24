8376e36c8754c307b7ef55fc38d85a0afcb33801
YinCY <yinchunyou2012@163.com>
2025-10-24 15:14:14 +0800

    molecular docking

--- molecular_docking/run_md.R
+++ molecular_docking/run_md.R
@@ -2,21 +2,34 @@ library(magrittr)
 library(fs)
 library(gluedocking)
 
+# my ubuntu
+# prepare_for_gluedocking(
+#   python_path = "/usr/bin/python",
+#   prepare_receptor_script = "/home/yincy/git/bior/functions/molecular_docking/prepare_receptor4.py",
+#   prepare_ligand_script = "/home/yincy/git/bior/functions/molecular_docking/prepare_ligand4.py",
+#   prepare_split_alt_script = "/home/yincy/git/bior/functions/molecular_docking/prepare_pdb_split_alt_confs.py",
+#   obabel_path = "/usr/bin/obabel",
+#   vina_path = "/usr/bin/vina"
+# )
+
+# CNS server
 prepare_for_gluedocking(
-  python_path = "/usr/bin/python",
-  prepare_receptor_script = "/home/yincy/git/bior/functions/molecular_docking/prepare_receptor4.py",
-  prepare_ligand_script = "/home/yincy/git/bior/functions/molecular_docking/prepare_ligand4.py",
-  prepare_split_alt_script = "/home/yincy/git/bior/functions/molecular_docking/prepare_pdb_split_alt_confs.py",
-  obabel_path = "/usr/bin/obabel",
-  vina_path = "/usr/bin/vina"
+  python_path = "/data1/yincy/miniconda3/envs/biopy/bin/python",
+  prepare_receptor_script = "/home/yincy/BioHome/github/functions/molecular_docking/prepare_receptor4.py",
+  prepare_ligand_script = "/home/yincy/BioHome/github/functions/molecular_docking/prepare_ligand4.py",
+  prepare_split_alt_script = "/home/yincy/BioHome/github/functions/molecular_docking/prepare_pdb_split_alt_confs.py",
+  obabel_path = "/data1/yincy/tools/openbabel/bin/obabel",
+  vina_path = "/data1/yincy/github/AutoDock-Vina-1.2.7/build/linux/release/vina", 
+  force = TRUE
 )
+
 
 pdb_ids <- c("1iep", "4hg7")
 cids <- c("2244", "5090")
 
 paths <- c("receptors/raw", 
     "receptors/trimmed",
-    "receptors/split_atl",
+    "receptors/split_alt",
     "receptors/prepared",
 
     "ligands/raw", 
@@ -35,7 +48,8 @@ pdb_files <- download_receptor(pdb_ids = pdb_ids,
 
 ligands_files <- download_ligand(
   cids = cids, 
-  output_dir = "ligands/raw"
+  output_dir = "ligands/raw", 
+  verify_ssl = TRUE
 )
 
 # step 1: remove non-protien atoms (water, ligands, etc)
