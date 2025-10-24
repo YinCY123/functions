5a12706127ea2a2b1a8824e8d71a9a267ee5eb8d
YinCY <yinchunyou2012@163.com>
2025-10-24 15:13:51 +0800

    cellchat

--- cellchat/cellchat_process.R
+++ cellchat/cellchat_process.R
@@ -4,17 +4,42 @@ cellchat_process <- function(cellchat,
     min_cell = 10, 
     ...){
         # loading required packages
-        suppressPackageStartupMessages(require(magrittr))
-        suppressPackageStartupMessages(require(CellChat))
+        suppressPackageStartupMessages(library(magrittr))
+        suppressPackageStartupMessages(library(CellChat))
 
-        cellchat <- cellchat %>% 
-            subsetData() %>% 
-            identifyOverExpressedGenes(do.fast = TRUE, min.cells = min_cell) %>% 
-            identifyOverExpressedInteractions() %>% 
-            computeCommunProb(type = type, trim = trim) %>% 
-            filterCommunication(min.cells = min_cell) %>% 
-            computeCommunProbPathway() %>% 
-            aggregateNet() %>% 
-            netAnalysis_computeCentrality()
+        # cellchat <- cellchat %>% 
+        #     subsetData() %>% 
+        #     identifyOverExpressedGenes(do.fast = TRUE, min.cells = min_cell) %>% 
+        #     identifyOverExpressedInteractions() %>% 
+        #     computeCommunProb(type = type, trim = trim) %>% 
+        #     filterCommunication(min.cells = min_cell) %>% 
+        #     computeCommunProbPathway() %>% 
+        #     aggregateNet() %>% 
+        #     netAnalysis_computeCentrality()
+
+        message("subsetData ...")
+        cellchat <- subsetData(cellchat)
+
+        message("identifyOverExpressedGenes ...")
+        cellchat <- identifyOverExpressedGenes(cellchat, do.faster = TRUE, min.cells = min_cell)
+
+        message("identifyOverExpressedInteractions ...")
+        cellchat <- identifyOverExpressedInteractions(cellchat)
+
+        message("computeCommunProb ...")
+        cellchat <- computeCommunProb(type = type, trim = trim)
+
+        message("filterCommunication ...")
+        cellchat <- filterCommunication(cellchat, min.cells = min_cell)
+
+        message("computeCommunProbPathway ...")
+        cellchat <- computeCommunProbPathway(cellchat)
+
+        message("aggregateNet ...")
+        cellchat <- aggregateNet(cellchat)
+
+        message("netAnalysis_computeCentrality ...")
+        cellchat <- netAnalysis_computeCentrality(cellchat)
+
         return(cellchat)
 }
