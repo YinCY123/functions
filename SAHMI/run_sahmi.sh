f13d6e98bf7868c55ca28bffea124160ccb3491d
YinCY <yinchunyou2012@163.com>
2025-12-26 22:32:48 +0800

    SAHMI

--- SAHMI/run_sahmi.sh
+++ SAHMI/run_sahmi.sh
@@ -1,88 +1,5 @@
-# fq_dir="/home/yincy/disk14/works/partime/2025/250418/GSE131882/unmapped"
-# output_dir="/home/yincy/disk14/works/partime/2025/250418/GSE131882/SAHMI"
-# samples=("SRR9141212_sorted" "SRR9141213_sorted" \
-#   "SRR9141214_sorted" "SRR9141215_sorted"\
-#   "SRR9141216_sorted" "SRR9141217_sorted" "SRR9141218_sorted" \
-#   "SRR9141219_sorted" "SRR9141220_sorted" "SRR9141221_sorted")
-# paired="TRUE"
+run_sahmi(){
 
-
-fq_dir="/home/yincy/disk14/works/partime/2025/250418/GSE131882/unmapped"
-output_dir="/home/yincy/disk14/works/partime/2025/250418/GSE131882/SAHMI"
-
-
-
-paired="TRUE"
-
-
-# 01 run kraken
-echo "run kraken..."
-for sample in "${samples[@]}"; do
-	echo "processing $sample..."
-	Rscript functions/run_kraken.r \
-		--sample "$sample" \
-		--fq1 $fq_dir/${sample}_R1.fastq.gz \
-		--fq2 $fq_dir/${sample}_R2.fastq.gz \
-		--out_path $output_dir/ \
-		--ncbi_blast_path /usr/lib/ncbi-blast+ \
-		--Kraken2Uniq_path /home/yincy/bin/kraken2 \
-		--kraken_database_path /home/yincy/ssd/kraken2/database/k2_minusb_20250402 \
-		--paired T \
-		--kreport2mpa_path functions/kreport2mpa.py
-done
-
-# 02 extrat microbiome reads
-echo "extract microbiome reads..."
-for sample in ${samples[@]}; do
-	echo "Processing the first file of $sample..."
-	Rscript functions/extract_microbiome_reads.r \
-		--sample_name "$sample" \
-		--fq $output_dir/${sample}_1.fq \
-		--kraken_report $output_dir/${sample}.kraken.report.txt \
-		--mpa_report $output_dir/${sample}.kraken.report.mpa.txt \
-		--out_path $output_dir/
-	mv $output_dir/${sample}.fa  $output_dir/${sample}_1.fa
-
-    if [[ "$paired" == "TRUE" ]]; then
-        echo "processing the second file of $sample..."
-
-        Rscript functions/extract_microbiome_reads.r \
-            --sample_name "$sample" \
-            --fq $output_dir/${sample}_2.fq \
-            --kraken_report $output_dir/${sample}.kraken.report.txt \
-            --mpa_report $output_dir/${sample}.kraken.report.mpa.txt \
-            --out_path $output_dir/
-        mv $output_dir/${sample}.fa  $output_dir/${sample}_2.fa 
-    fi
-done
-
-# extract microbiome output
-
-echo "extract microbiome output..."
-for sample in "${samples[@]}"; do
-	echo ""
-	echo "Processing $sample..."
-	echo ""
-	kraken_report="$output_dir/${sample}.kraken.report.txt"
-	mpa_report="$output_dir/${sample}.kraken.report.mpa.txt"
-	if [[ -f "$kraken_report" && -f "$mpa_report" ]]; then
-		Rscript functions/extract_microbiome_output.r \
-			--sample_name "$sample" \
-			--output_file $output_dir/${sample}.kraken.output.txt \
-			--kraken_report "$kraken_report" \
-			--mpa_report "$mpa_report" \
-			--out_path $output_dir/
-	else
-		echo "Missing input files for $sample. Skipping."
-	fi
-	echo ""
-	echo "Sample $sample has been processed..."
-	echo ""
-done
-
-# 03 single cell kmer analysis
-for sample in "${samples[@]}"; do
-	if [[ "$paired" == TRUE ]]; then
 	if [[ "$1" == "--help" || "$1" == "-h" ]]; then
 		echo "Usage: run_shami [options]"
 		echo ""
@@ -151,7 +68,7 @@ for sample in "${samples[@]}"; do
 			--out_path $output_dir/ \
 			--ncbi_blast_path /usr/lib/ncbi-blast+ \
 			--Kraken2Uniq_path /home/yincy/bin/kraken2 \
-			--kraken_database_path /home/yincy/BioHome/datasets/kraken2/k2_standard_20250714 \
+			--kraken_database_path /home/yincy/ssd/kraken2/database/k2_minusb_20250402 \
 			--paired T \
 			--kreport2mpa_path "python3 functions/kreport2mpa.py"
 	done
@@ -251,3 +168,4 @@ for sample in "${samples[@]}"; do
 			--cb_len 16 \
 			--umi_len 10
 		done
+}
