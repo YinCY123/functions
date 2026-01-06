d39893647bbf0fff4980ea197af6a33ff0c946f1
YinCY <yinchunyou2012@163.com>
2026-01-06 14:18:37 +0800

    Giotto

--- Giotto/normalize_spat_location.R
+++ Giotto/normalize_spat_location.R
@@ -5,9 +5,10 @@ normalize_spat_location <- function(gobject, factor = 1000){
         ((x - min(x)) / (max(x) - min(x))) * factor
     }
 
-    tmp <- gobject %>% getSpatialLocations(output = "data.table")
+    tmp <- gobject %>% getSpatialLocations(output = "data.table") %>% as.data.frame()
     tmp[[1]] <- scales(tmp[[1]])
     tmp[[2]] <- scales(tmp[[2]])
+    # tmp <- apply(tmp, 2, scales)
 
     # convert to spatLocsObj
     spatLoc <- createSpatLocsObj(tmp, name = "raw", spat_unit = "cell")
