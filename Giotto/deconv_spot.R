e091d84e57018781b5ae20b157b9f0f73e8fd8d1
YinCY <yinchunyou2012@163.com>
2026-06-12 10:21:51 +0800

    Giotto

--- Giotto/deconv_spot.R
+++ Giotto/deconv_spot.R
@@ -3,28 +3,37 @@ deconv_spot <- function(sces,
                         sc_cell_id_column,
                         sc_celltype_column,
                         sc_gene_id_column,
+                        filter.mt.genes = "^mt-",
                         st_spot_ID_column = "cell_ID",
                         st_gene_ID_column = "feat_ID",
                         project = "ddls",
                         threads = 8,
+                        output_dir = NULL,
                         ...){
   # loading required packages
-  library(qs2)
+  library(qs)
   library(SpatialDDLS)
   library(SpatialExperiment)
   
+  if(is.null(output_dir)){
+    output_dir = "./"
+  }
+
   # create spatialDDLS object
+  message("create spatialDDLS object...")
   ddls <- createSpatialDDLSobject(
     sc.data = sces, 
     sc.cell.ID.column = sc_cell_id_column, 
     sc.cell.type.column = sc_celltype_column, 
     sc.gene.ID.column = sc_gene_id_column, 
+    filter.mt.genes = filter.mt.genes,
     st.data = spes, 
     st.spot.ID.column = st_spot_ID_column, 
     st.gene.ID.column = st_gene_ID_column, 
     project = project
   )
   
+  message("generate gene cell mix profile...")
   ddls <- genMixedCellProp(
     object = ddls, 
     cell.ID.column = sc_cell_id_column, 
@@ -33,18 +42,21 @@ deconv_spot <- function(sces,
     n.cells = 50
   )
   
+  message("simulation mixed profile...")
   ddls <- simMixedProfiles(
     object = ddls, 
     type.data = "both", 
     threads = threads
   )
-  
+  message("training the prediction model...")
   ddls <- trainDeconvModel(
     object = ddls, 
     type.data.train = "mixed", 
     threads = threads
   )
+  qsave(ddls, file = paste0(output_dir, "ddls.qs"))
   
+  message("deconvolution spots...")
   ddls <- deconvSpatialDDLS(
     object = ddls, 
     index.st = 1, 
