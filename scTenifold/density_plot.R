4ab176b355b6ce72c027a842bd6233715747327d
YinCY <yinchunyou2012@163.com>
2026-07-20 20:25:58 +0800

    scTenifold

---
+++ scTenifold/density_plot.R
@@ -0,0 +1,72 @@
+density_plot <- function(x, 
+    variable, 
+    fill_color = NULL,
+    x_name = "Z score",
+
+    # density params
+    bw = "nrd0", 
+    adjust = 1, 
+    kernel = "gaussian", 
+    weight = NULL, 
+    window = "gaussian",
+
+    # geom_ribbon
+    lower = -1, 
+    upper = 1, 
+    scale_y = 1.2,
+    ...
+    ){
+
+    # loading required packages
+    library(ggplot2)
+    library(MetBrewer)
+    library(magrittr)
+
+    # approximate density
+    dens <- density(x = x[[variable]], bw = bw, adjust = adjust, kernel = kernel, weight = weight)
+
+    # model a function
+    model <- approxfun(x = dens$x, y = dens$y, yleft = 0, yright = 0, method = "linear")
+
+    lower_df = data.frame(x = dens$x[dens$x < lower], y = model(dens$x[dens$x < lower]))
+    lower_df$group <- "lower"
+
+    middle <- data.frame(x = dens$x[dens$x >= lower & dens$x <= upper], 
+        y = model(dens$x[dens$x >= lower & dens$x <= upper]))
+    middle$group <- "middle"
+
+    upper_df = data.frame(x = dens$x[dens$x > upper], y = model(dens$x[dens$x > upper]))
+    upper_df$group <- "upper"
+
+    anno_df <- Reduce(rbind, list(lower_df, middle, upper_df))
+
+    if(is.null(fill_color)){
+        cols <- met.brewer("Java", 2)
+        fill_color <- c(cols[1], "grey", cols[2])
+        names(fill_color) <- c("lower", "middle", "upper")
+    }
+
+    # up- and down-regulated genes
+    num_down <- sum(x[[variable]] < lower)
+    num_up <- sum(x[[variable]] > upper)
+
+
+    x_label <- c(seq(floor(min(anno_df$x)), 0, 1), seq(0, ceiling(max(anno_df$x)), 1))
+
+    p <- anno_df %>% 
+        ggplot(aes(x, y)) +
+        geom_ribbon(aes(ymin = 0, ymax = y, fill = group)) +
+        scale_fill_manual(values = fill_color) +
+        scale_x_continuous(name = x_name, breaks = x_label) +
+        scale_y_continuous(name = "Density") +
+        annotate(geom = "text", 
+            x = min(anno_df$x), 
+            y = max(anno_df$y) * scale_y, 
+            color = fill_color[c(1,3)],
+            label = paste0("number of down-regulated genes: ", num_down, "\nnumber of up-regulated genes: ", num_up), 
+            hjust = "inward") +
+        theme(panel.background = element_blank(), 
+            panel.border = element_rect(fill = NA), 
+            ...)
+    return(p)
+}
---
+++ scTenifold/scatter_plot.R
@@ -0,0 +1,60 @@
+scatter_plot <- function(df, 
+    x_name = "Z score", 
+    y_name = "-log10(p value)",
+    upper = 1, 
+    lower = -1,
+    point_colors = c("#663171", "grey", "#ea7428"), 
+    point_size = 1, 
+    linecolor = "blue",
+    text_size = 3, 
+    legend.key.size = 3,
+    linewidth = 0.5,
+    ...){
+    # loading required packages
+    library(magrittr)
+    library(ggplot2)
+    library(MetBrewer)
+    library(ggrepel)
+
+
+    # prepare plotting data
+    df <- df %>% 
+        dplyr::mutate(direction = ifelse(Z > 0, "Z > 0", "Z <= 0"), 
+        sig = ifelse(Z > upper, "up", ifelse(Z < lower, "down", "ns")), 
+        y = -log10(p.value))
+    
+    # anno
+    anno_up <- df %>% dplyr::filter(Z>upper) %>% dplyr::arrange(desc(y)) %>% dplyr::slice_head(n = 5)
+    anno_down <- df %>% dplyr::filter(Z<lower) %>% dplyr::arrange(desc(y)) %>% dplyr::slice_head(n = 5)
+    anno <- rbind(anno_up, anno_down)
+
+    if(!is.null(point_colors)){
+        names(point_colors) = c("down", "ns", "up")
+    }else{
+        point_color = met.brewer("Java", 2)
+        point_colors <- c(point_color[1], "grey", point_color[2])
+        names(point_color) = c("down", "ns", "up")
+    }
+    p <- df %>% 
+        ggplot(aes(Z, y)) +
+        geom_point(aes(color = sig), size = point_size) +
+        geom_text_repel(data = anno, aes(label = Gene), size = text_size) +
+        geom_smooth(aes(x = Z, y = y), 
+            se = F, 
+            linetype = 2,
+            linewidth = linewidth, 
+            color = linecolor,
+            method = glm, 
+            method.args = list(family = "binomial")) +
+        facet_wrap(vars(direction), nrow = 1) +
+        scale_x_continuous(name = x_name) +
+        scale_y_continuous(name = y_name, expand = expansion(mult = c(0.01, 0.1))) +
+        scale_color_manual(name = NULL, values = point_colors) +
+        guides(color = guide_legend(override.aes = list(size = legend.key.size))) +
+        theme(panel.background = element_blank(), 
+            panel.border = element_rect(fill = NA), 
+            strip.background = element_blank(), 
+            ...)
+
+    return(p)
+}
